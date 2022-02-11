5db8cd6bd5c6936121a2d4d5393baf3180855976
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2022-02-11 22:42:12 +0800

    Update Keys.py

--- RSA_Make/Keys.py
+++ RSA_Make/Keys.py
@@ -1,23 +1,23 @@
-# from pyasn1.type import univ, namedtype
+from pyasn1.type import univ, namedtype
 
-# class RSAPublicKey(univ.Sequence):
-#     componentType = namedtype.NamedTypes(
-#         namedtype.NamedType('modulus', univ.Integer()),
-#         namedtype.NamedType('publicExponent', univ.Integer())
-#     )
+class RSAPublicKey(univ.Sequence):
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType('modulus', univ.Integer()),
+        namedtype.NamedType('publicExponent', univ.Integer())
+    )
 
-# class RSAPrivateKey(univ.Sequence):
-#     '''
-#     Abiding to RFC 2313 Standards for RSAPrivateKey's ASN1 Structure
-#     '''
-#     componentType = namedtype.NamedTypes(
-#         namedtype.NamedType('version', univ.Integer()),
-#         namedtype.NamedType('modulus', univ.Integer()),
-#         namedtype.NamedType('publicExponent', univ.Integer()),
-#         namedtype.NamedType('privateExponent', univ.Integer()),
-#         namedtype.NamedType('prime1', univ.Integer()),
-#         namedtype.NamedType('prime2', univ.Integer()),
-#         namedtype.NamedType('exponent1', univ.Integer()),
-#         namedtype.NamedType('exponent2', univ.Integer()),
-#         namedtype.NamedType('coefficient', univ.Integer())
-#     )
+class RSAPrivateKey(univ.Sequence):
+    '''
+    Abiding to RFC 2313 Standards for RSAPrivateKey's ASN1 Structure
+    '''
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType('version', univ.Integer()),
+        namedtype.NamedType('modulus', univ.Integer()),
+        namedtype.NamedType('publicExponent', univ.Integer()),
+        namedtype.NamedType('privateExponent', univ.Integer()),
+        namedtype.NamedType('prime1', univ.Integer()),
+        namedtype.NamedType('prime2', univ.Integer()),
+        namedtype.NamedType('exponent1', univ.Integer()),
+        namedtype.NamedType('exponent2', univ.Integer()),
+        namedtype.NamedType('coefficient', univ.Integer())
+    )
